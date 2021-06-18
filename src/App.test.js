4b10674b38fc8a1aed54973d56a73b9186dec38f
ivanburda1986 <burda.ivan@gmail.com>
2021-06-18 12:14:07 +0200

    implemented button state change test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn testing library/i);
-  expect(linkElement).toBeInTheDocument();
+test("button has the correct initial color", () => {
+  render(<App/>);
+
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', {name: 'Change color to blue'});
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({backgroundColor: 'red'});
 });
+
+
+test("button turns blue when clicked and gets the correct title", ()=>{
+  render(<App/>);
+  const colorButton = screen.getByRole('button', {name: 'Change color to blue'});
+
+  // click the button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({backgroundColor: 'blue'});
+
+  // expect the button to have the title saying clicking it will change the color to red
+  expect(colorButton.textContent).toBe('Change color to red');
+
+});
+
+
