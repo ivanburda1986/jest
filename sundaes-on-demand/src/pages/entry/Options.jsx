0be5a4ff4a98821952fc5b0fe933f725007345d5
ivanburda1986 <burda.ivan@gmail.com>
2021-06-21 00:27:46 +0200

    getting data from the server

--- sundaes-on-demand/src/pages/entry/Options.jsx
+++ sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,3 +1,16 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [optionType]);
+
   return <div>Options</div>;
 }
