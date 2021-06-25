396de938f805af82c96810910e6f1345a9acacc2
ivanburda1986 <burda.ivan@gmail.com>
2021-06-26 01:04:13 +0200

    finished the crash course

--- brad-traversy-crashcourse/functions.js
+++ brad-traversy-crashcourse/functions.js
@@ -1,6 +1,19 @@
+const { default: axios } = require("axios");
+
 const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = { firstName: "Brad" };
+    user["lastName"] = "Traversy";
+    return user;
+  },
+  fetchUser: () =>
+    axios
+      .get("http://jsonplaceholder.typicode.com/users/1")
+      .then((res) => res.data)
+      .catch((err) => "error"),
 };
 
 module.exports = functions;
--- brad-traversy-crashcourse/functions.test.js
+++ brad-traversy-crashcourse/functions.test.js
@@ -1,5 +1,32 @@
 const functions = require("./functions");
 
+// const initDatabase = () => console.log("Database initialized...");
+// const closeDatabase = () => console.log("Database closed...");
+
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+const nameCheck = () => {
+  console.log("Checking name ...");
+};
+
+describe("Checking names", () => {
+  beforeEach(() => nameCheck());
+
+  test("User is Jeff", () => {
+    const user = "Jeff";
+    expect(user).toBe("Jeff");
+  });
+
+  test("User is Karen", () => {
+    const user = "Karen";
+    expect(user).toBe("Karen");
+  });
+});
+
 test("Adds 2+2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
@@ -10,4 +37,45 @@ test("Adds 2+2 to NOT equal 5", () => {
 
 test("Should be null", () => {
   expect(functions.isNull()).toBeNull();
+});
+
+test("Should be falsy", () => {
+  expect(functions.checkValue(null)).toBeFalsy(); //also: toBeTruthy
+});
+
+test("User should be Brad Traversy object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Brad",
+    lastName: "Traversy",
+  });
+});
+
+test("Shuold be under 1600", () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600); //toBeLessThankOrEqueal
+});
+
+test("There is no I in team", () => {
+  expect("tieam").not.toMatch(/I/);
+});
+
+test("Admi should be in usernames", () => {
+  usernames = ["John", "Karen", "admin"];
+  expect(usernames).toContain("admin");
+});
+
+//Working with async data - Promise
+test("User fetched name should be Leanne Graham", () => {
+  expect.assertions(1);
+  return functions.fetchUser().then((data) => {
+    expect(data.name).toEqual("Leanne Graham");
+  });
+});
+
+//Working with async data - Async await
+test("User fetched name should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
 });
